d414f4ca61c8530a6b554801544d4f7650409e86
Abo-Zeyd <azzedd.aoui@gmail.com>
2025-10-22 23:07:59 +0100

    modifie' d221020252304'

--- app/page.tsx
+++ app/page.tsx
@@ -19,15 +19,20 @@ export default function Home() {
         <div className="text-center max-w-md text-xl">
           <p>الموقع لا يحتوي حاليا الا على التوزيعات السنوية والشهرية محينة لسنة 2025-2026 </p>
           <button>
-            <Link href="/pages/monthlyDistrubtion" className="underline text-blue-600">من هنا</Link>
+            <Link href="/pages/monthlyDistrubtion" className=" text-blue-600">من هنا</Link>
           </button>
+
           <p>وإن شاء الله سيتم إثراؤه وإتمامه بما يسهل العملية التعليمية حسب الجهد والوقت </p>
-          <Link href="https://www.youtube.com/watch?v=E72wi7bpoRM" className="underline text-blue-600">شرح إنجاز التوازيع الجزء الأول</Link>
+          <div className="flex flex-col gap-4 mt-4 no-underline">
+            <Link href="https://www.youtube.com/watch?v=E72wi7bpoRM" className=" text-blue-600">شرح إنجاز التوازيع الجزء الأول</Link>
+
+            <Link href="https://www.youtube.com/watch?v=_LA2sr9aQfw" className=" text-blue-600">شرح إنجاز التوازيع الجزء الثاني</Link>
+          </div>
         </div>
 
       </div>
     </div>
 
-    
+
   );
 }
